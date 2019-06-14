5e3238f3d648a88c09911c08ac3bb756eb6c063d
henryaraujo <luizhenryaraujo@gmail.com>
2019-06-14 16:39:42 -0300

    created test with jest

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # invillia-test-node
+
+## Instalação
+Todas as dependencias foram instaladas utilizando o `npm` podendo ser instaladas a partir do comando:
+> `npm i ou npm install`
+
+## Tecnologias Usadas
+[KoaJS](https://koajs.com/) para criação da Api
+
+[Babel](https://babeljs.io/) para compilar codigos ES6/ES7
+
+[NeDB](https://github.com/louischatriot/nedb) para persistência de dados (similar ao MongoDB)
+
+[Jest](https://github.com/louischatriot/nedb) para cobertura de testes
+
+[Postman](https://www.getpostman.com/) para realizar requisições a API
+
+## Requisitos
+node: LTS
+
+npm: 6.9.0
+
+## Preparando o desenvolvimento
+Para rodar o projeto em sua máquina, execute os seguintes comandos:
+
+`git clone https://github.com/henryaraujo/invillia-test-node`
+
+`cd invillia-test-node/`
+
+`npm install`
+
+`npm start`
+
+> O servidor será iniciado na seguinte URL: `http://localhost:8000/v1/api`
+
+## Observações
+Para navegar entre as rotas será necessario:
+
+Criar usuário na rota: `http://localhost:8000/v1/api/auth/signup`
+
+Realizar autenticação na rota: `http://localhost:8000/v1/api/auth/signin`
+
+Utilizar o token [JWT](https://jwt.io/) gerado ao realizar autenticação
+
+## Usando Postman
+
+Importar collection [InvilliaTestCollection](https://www.getpostman.com/collections/cd374fead3edc793c231)
+
+## Licença
+O projeto utiliza a licença ISC descrita no link [LICENSE](https://pt.wikipedia.org/wiki/Licen%C3%A7a_ISC).
--- src/__tests__/routes.test.js
+++ src/__tests__/routes.test.js
@@ -1,12 +1,144 @@
-import supertest from 'supertest';
+import request from 'supertest';
+import app from '../app';
 
-beforeAll(async () => {
-    console.log('start invillia node-test')
-});
+const URL = '/v1/api/';
+var token = '';
 
+describe('POST /auth/signin', () => {
 
-describre('test', () => {
-    test('send post to router auth', async() => {
-
-    })
+    test('expect token user', async () => {
+        const user = {
+            email: 'henryaraujo@email.com',
+            password: '123456'
+        }
+        const res = await request(app.callback()).post(`${URL}auth/signin`).send(user);
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty("token");
+        token = res.body.token
+    });
 })
+
+
+
+describe('GET routes api', () => {
+
+    test('expect list player', async () => {
+        const res = await request(app.callback())
+                        .get(`${URL}players`)
+                        .set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(200);
+        expect(res.type).toEqual("application/json");
+        expect(res.body).toEqual(
+            expect.arrayContaining([]),
+        );
+    });
+
+
+    test('expect list tournaments', async () => {
+        const res = await request(app.callback())
+                        .get(`${URL}tournaments`)
+                        .set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual(
+            expect.arrayContaining([]),
+        );
+    });
+
+
+    test('expect list phases', async () => {
+        const res = await request(app.callback())
+                        .get(`${URL}phases`)
+                        .set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual(
+            expect.arrayContaining([]),
+        );
+    });
+
+
+    test('expect list results', async () => {
+        const res = await request(app.callback())
+                        .get(`${URL}phases`)
+                        .set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual(
+            expect.arrayContaining([]),
+        );
+    });
+})
+
+
+describe('POST routes api', () => {
+
+    test('create new player', async () => {
+        const player = {
+            name: "Stewie", 
+            nicknam: "stewk2", 
+            level: 20,
+            is_active: true
+        }
+        const res = await request(app.callback())
+                        .post(`${URL}players`)
+                        .set('Authorization', `Bearer ${token}`)
+                        .send(player);
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty("_id");
+    });
+
+
+    test('create new tournament', async () => {
+        const player ={
+            name: "CS GO for Newbies", 
+            description: "Rank Prata mode=on", 
+            year: "2019",
+            game: "CS GO",
+            is_active: true,
+            phases : [
+                "Dq7OJv7fkhJWpYoi",
+                "YCvs2w4NqfWHS8ZY"
+            ]
+        }
+        const res = await request(app.callback())
+                        .post(`${URL}tournaments`)
+                        .set('Authorization', `Bearer ${token}`)
+                        .send(player);
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty("_id");
+    });
+
+    test('create new phase', async () => {
+        const phase = {
+            name: "Oitavas",
+            description: "Melhor de 3",
+            max_players: 10,
+            is_active: true,
+            players: [
+                "Zva5rKoAg2qvzrIt",
+                "l5ZndOcwx4ZZgEOc",
+                "mn6VWYysfQdOvLlN"
+             ]
+        }
+        const res = await request(app.callback())
+                        .post(`${URL}phases`)
+                        .set('Authorization', `Bearer ${token}`)
+                        .send(phase);
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty("_id");
+    });
+
+    test('create new results', async () => {
+        const result ={
+            player_id: "sN0sMEhn4H3NabfT", 
+            phase_id: "Dq7OJv7fkhJWpYoi", 
+            points: 80,
+            is_active: true
+        }
+        const res = await request(app.callback())
+                        .post(`${URL}phases`)
+                        .set('Authorization', `Bearer ${token}`)
+                        .send(result);
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty("_id");
+    });
+})
+
